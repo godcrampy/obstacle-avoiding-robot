34f4da6bab1f18608b64dd5eaf41f88403715cec
Sahil Bondre <sahilbondre@gmail.com>
2019-01-28 13:57:03 +0530

    First Commit

---
+++ obstacle-avoiding-robot/main.c
@@ -0,0 +1,18 @@
+/*
+ * obstacle-avoiding-robot.c
+ *
+ * Created: 28-01-2019 13:40:59
+ * Author : SAHIL
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
