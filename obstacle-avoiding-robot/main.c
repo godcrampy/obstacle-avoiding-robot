e32114f421ca1bf9509e1e1bebb3ccbf43583670
Sahil Bondre <sahilbondre@gmail.com>
2019-01-30 19:52:17 +0530

    Fixed main.c code

--- obstacle-avoiding-robot/main.c
+++ obstacle-avoiding-robot/main.c
@@ -13,11 +13,13 @@ int is_white(int n)
 	return bit_is_clear(PINA,n);//returns true if there is white line under
 }
 
+
 void pwm_init(){
 	TCCR0|=(1<<WGM00)|(1<<WGM01); //Set PWM to Fast PWM Mode
 	TCCR0|=(1<<COM00)|(1<<COM01); //Set Fast PWM to inverting mode
-	TCCR0|=(1<<CS01) //Prescale 8 bit counter to prescaling of 8 to count till 2048 counting (>100)(coz ICR1 is 1000 and hence couter must be greater than 1000)
-	TCCR0&=~(1<<CS02)
+	TCCR0 |=(1<<CS01); //Prescale 8 bit counter to prescaling of 8 to count till 2048 counting (>100)(coz ICR1 is 1000 and hence couter must be greater than 1000)
+	    
+	TCCR0 &=~(1<<CS02);
 	ICR1=1000; //Sets ICP1(Reference to 1000)(Although it is called ICP1
 }
 
@@ -81,103 +83,107 @@ int main(void)
     
 	 DDRA=0x00;
 	 DDRD=0xFF;
+	 pwm_init();
+	 
 	//Code for other ports I/O not set
     while (1)
     {
 		//Stop and Reverse
 	if(is_white(0) && is_white(1) && is_white(2) && is_white(3) && is_white(4) && is_white(5) && is_white(6) && is_white(7)){
-		stop()
+		stop();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		move_backward()
+		move_backward();
 	}
 		//4 White Code
 	else if(is_white(0) && is_white(1) && is_white(2) && is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-			turn_right_hard()
+			turn_right_hard();
 		}
 	else if(!is_white(0) && is_white(1) && is_white(2) && is_white(3) && is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-			turn_right()
+			turn_right();
     }
 	else if(!is_white(0) && !is_white(1) && is_white(2) && is_white(3) && is_white(4) && is_white(5) && !is_white(6) && !is_white(7)){
-		move_forward()
+		move_forward();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && is_white(3) && is_white(4) && is_white(5) && is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && is_white(4) && is_white(5) && is_white(6) && is_white(7)){
-		turn_right_hard()
+		turn_right_hard();
 	}
 	//3 White Code
 	else if(is_white(0) && is_white(1) && is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right_hard()
+		turn_right_hard();
 	}
 	else if(!is_white(0) && is_white(1) && is_white(2) && is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && is_white(2) && is_white(3) && is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && is_white(3) && is_white(4) && is_white(5) && !is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && is_white(4) && is_white(5) && is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && is_white(5) && is_white(6) && is_white(7)){
-		turn_left_hard()
+		turn_left_hard();
 	}
 	//2 white code
 	else if(is_white(0) && is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right_hard()
+		turn_right_hard();
 	}
 	else if(!is_white(0) && is_white(1) && is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && is_white(2) && is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && is_white(3) && is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		move_forward()
+		move_forward();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && is_white(4) && is_white(5) && !is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && is_white(5) && is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && is_white(6) && is_white(7)){
-		turn_left_hard()
+		turn_left_hard();
 	}
 	//1 line code
 	else if(is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right_hard()
+		turn_right_hard();
 	}
 	else if(!is_white(0) && is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_right()
+		turn_right();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && is_white(4) && !is_white(5) && !is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && is_white(5) && !is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && is_white(6) && !is_white(7)){
-		turn_left()
+		turn_left();
 	}
 	else if(!is_white(0) && !is_white(1) && !is_white(2) && !is_white(3) && !is_white(4) && !is_white(5) && !is_white(6) && is_white(7)){
-		turn_left_hard()
+		turn_left_hard();
 	}
 	
 	
 	
+	}
+	return 0;
 }
 
